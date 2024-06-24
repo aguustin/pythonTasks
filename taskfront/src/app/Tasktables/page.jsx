efed04fad0de0042a21f92258406679b8a368fbb
aguustin <agustin.molee@gmail.com>
2024-06-24 13:40:24 -0300

    hacer en casa

--- taskfront/src/app/Tasktables/page.jsx
+++ taskfront/src/app/Tasktables/page.jsx
@@ -1,11 +1,13 @@
+import pruebaImg from '../assets/prueba.jpg'
+
 function TaskTables() {
     return(
         <section className="bg-slate-50 h-screen p-6">
-            <div className="text-black w-72 bg-slate-800">
-                <div>
+            <div className="text-black w-72 bg-slate-800 text-center shadow-xl shadow-slate-400 rounded-md border-black">
+                <div className="h-10 w-full bg-slate-50">
                     <label>Table 1</label>
                 </div>
-                <img src="" alt=""></img>
+                <img src={pruebaImg.src} alt=""></img>
                 <div>
                     <label>2024-02-16</label>
                 </div>
