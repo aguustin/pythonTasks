966d0dd8548797f709c4a198acbc964af7a985b8
aguustin <agustin.molee@gmail.com>
2025-11-03 21:11:13 -0300

    cambiando la url .env de api front

--- taskfront/api/taskRequest.js
+++ taskfront/api/taskRequest.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-export const getUserTablesRequest = (sessionId) => axios.get(`http://127.0.0.1:8000/get_user_tables/${sessionId}`)
+export const getUserTablesRequest = (sessionId) => axios.get(`${NEXT_PUBLIC_BACK_URL}/get_user_tables/${sessionId}`)
 
-export const getTableRequest = (tableId) => axios.get(`http://127.0.0.1:8000/get_table/${tableId}`)
+export const getTableRequest = (tableId) => axios.get(`${NEXT_PUBLIC_BACK_URL}/get_table/${tableId}`)
 
-export const saveTableRequest = (data) => axios.post('http://127.0.0.1:8000/create_tasks_tables/', data)
+export const saveTableRequest = (data) => axios.post(`${NEXT_PUBLIC_BACK_URL}/create_tasks_tables/`, data)
 
-export const updateTableRequest = (taskTableId, tableTitle) => axios.post('http://127.0.0.1:8000/update_tasks_table/', {taskTableId, tableTitle})
+export const updateTableRequest = (taskTableId, tableTitle) => axios.post(`${NEXT_PUBLIC_BACK_URL}/update_tasks_table/`, {taskTableId, tableTitle})
 
-export const createTaskRequest = (data) => axios.post('http://127.0.0.1:8000/create_task/', data)
+export const createTaskRequest = (data) => axios.post(`${NEXT_PUBLIC_BACK_URL}/create_task/`, data)
 
-export const updateTaskRequest = (data) => axios.post('http://127.0.0.1:8000/update_tasks/', data)
+export const updateTaskRequest = (data) => axios.post(`${NEXT_PUBLIC_BACK_URL}/update_tasks/`, data)
 
-export const deleteTaskRequest = (taskId) => axios.delete(`http://127.0.0.1:8000/delete_tasks/${taskId}`) 
+export const deleteTaskRequest = (taskId) => axios.delete(`${NEXT_PUBLIC_BACK_URL}/delete_tasks/${taskId}`) 
--- taskfront/api/userRequests.js
+++ taskfront/api/userRequests.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const signInRequest = async (data) => await axios.post('http://127.0.0.1:8000/create_user/', data)
+export const signInRequest = async (data) => await axios.post(`${NEXT_PUBLIC_BACK_URL}/create_user/`, data)
 
-export const logInRequest = async (mail, password) => await axios.get(`http://127.0.0.1:8000/get_credentials/${mail}/${password}`)
+export const logInRequest = async (mail, password) => await axios.get(`${NEXT_PUBLIC_BACK_URL}/get_credentials/${mail}/${password}`)
 
-export const deleteRequest = async (userId) => await axios.delete(`http://127.0.0.1:8000/delete_user/${userId}`)
+export const deleteRequest = async (userId) => await axios.delete(`${NEXT_PUBLIC_BACK_URL}/delete_user/${userId}`)
