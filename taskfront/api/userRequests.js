208631fc7397c0d3e4ed3b45882d956626f2c990
aguustin <agustin.molee@gmail.com>
2025-11-03 22:09:19 -0300

    cambiando la url .env de api front

--- taskfront/api/userRequests.js
+++ taskfront/api/userRequests.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const signInRequest = async (data) => await axios.post(`${NEXT_PUBLIC_BACK_URL}/create_user/`, data)
+export const signInRequest = async (data) => await axios.post(`${process.env.NEXT_PUBLIC_BACK_URL}/create_user/`, data)
 
-export const logInRequest = async (mail, password) => await axios.get(`${NEXT_PUBLIC_BACK_URL}/get_credentials/${mail}/${password}`)
+export const logInRequest = async (mail, password) => await axios.get(`${process.env.NEXT_PUBLIC_BACK_URL}/get_credentials/${mail}/${password}`)
 
-export const deleteRequest = async (userId) => await axios.delete(`${NEXT_PUBLIC_BACK_URL}/delete_user/${userId}`)
+export const deleteRequest = async (userId) => await axios.delete(`${process.env.NEXT_PUBLIC_BACK_URL}/delete_user/${userId}`)
